6b650103acc19c60dfb529465ec1660aa6c9d520
“bbaatten” <morten@mscp.dk>
2020-09-03 17:46:42 +0300

    new release

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import type { KmlMarker } from 'react-native-maps';
-import { Button } from 'react-native-elements';
+import { Button, withTheme } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 /*
@@ -39,30 +39,52 @@ class HomeScreen extends React.Component<{}, any> {
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <MapView initialRegion={{latitude: 49.99229117,longitude: 36.23333931,latitudeDelta: 0.00051,longitudeDelta: 0.05,}} style={{ flex: 1, height: 400, width: '100%', }} showsUserLocation={true}>
+        <MapView initialRegion={{ latitude: 49.99229117, longitude: 36.23333931, latitudeDelta: 0.00051, longitudeDelta: 0.05, }} style={{ flex: 1, height: 400, width: '100%', }} showsUserLocation={true}>
           {this.state.markers.map((marker: KmlMarker, index: any) => (
+
             <Marker key={index} title={marker.title as string} description={marker.description as string} coordinate={marker.coordinate}>
-              <Callout>
-                <View style={{padding: 10 }}>
-                <Text style={styles.titleText}>Tom Jensen Johnson</Text>
-                <Text style={styles.baseText}>Turns 65 in 4 months</Text>
-                <Text style={styles.baseText}>8 miles away</Text>
-                <View style={{ flexDirection: 'row', marginTop:20 }}>
-                
-                <Button style={{marginRight:6}} onPress={this.test} title="" icon={
-    <Icon
-      name="car"
-      size={18}
-      style={{padding:3}}
-      color="white"
-    />
-  }/><Button style={{marginLeft:6}} onPress={this.test} title="See More Info"/>
+              <Callout tooltip>
+                <View style={{
+                  borderTopStartRadius: 15, borderTopRightRadius: 15, padding: 20, backgroundColor: 'white',
+                  shadowColor: 'black', shadowOpacity: 0.15, shadowRadius: 5, shadowOffset: { width: 5, height: 50 }
+                }}>
+                  <Text style={styles.titleText}>Tom Jensen Johnson</Text>
+                  <Text style={styles.baseText}>Turns 65 in 4 months</Text>
+                  <Text style={styles.baseText}>8 miles away</Text>
+
                 </View>
-                </View>
+                <Button buttonStyle={{
+                  borderTopStartRadius: 0,
+                  borderTopRightRadius: 0
+                }} containerStyle={{
+                  borderTopStartRadius: 0,
+                  borderTopRightRadius: 0,
+                  borderBottomLeftRadius: 15,
+                  borderBottomRightRadius: 15
+                }} onPress={this.test} title="Get Directions" icon={
+                  <Icon
+                    name="car"
+                    size={18}
+                    style={{ padding: 3, marginRight: 5 }}
+                    color="white"
+                  />
+                } />
               </Callout>
             </Marker>
           ))}
         </MapView>
+        <Button buttonStyle={{ backgroundColor: 'white',          shadowColor: 'black',
+          shadowOpacity: 0.75,
+          shadowRadius: 1,
+          shadowOffset: { width: 15, height: 15 }, }} onPress={this.test} icon={
+          <Icon name="car" size={18} style={{ padding: 3 }} color="black" />}
+          containerStyle={{
+
+          position: "absolute",
+          left: 30,
+          borderRadius: 50,
+          top: 50, backgroundColor: 'white;'
+        }} />
       </View>
     );
   }
@@ -78,25 +100,27 @@ export default class App extends React.Component {
     return (
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} options={{headerRight: () => (
-            <Button onPress={() => alert('This is a button!')} title="" type='clear' icon={
-              <Icon
-                name="cloud-download"
-                size={20}
-                style={{padding:3}}
-                
-              />
-            }/>
-          ),headerLeft: () => (
-            <Button onPress={() => alert('This is a button!')} title="" type='clear' icon={
-              <Icon
-                name="user"
-                size={20}
-                style={{padding:5}}
-                
-              />
-            }/>
-          )}
+          <Stack.Screen name="Home" component={HomeScreen} options={{
+            headerShown: false, headerRight: () => (
+              <Button onPress={() => alert('This is a button!')} title="" type='clear' icon={
+                <Icon
+                  name="cloud-download"
+                  size={20}
+                  style={{ padding: 3 }}
+
+                />
+              } />
+            ), headerLeft: () => (
+              <Button onPress={() => alert('This is a button!')} title="" type='clear' icon={
+                <Icon
+                  name="user"
+                  size={20}
+                  style={{ padding: 5 }}
+
+                />
+              } />
+            )
+          }
 
           } />
         </Stack.Navigator>
