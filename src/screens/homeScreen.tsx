11c328a9d4ae4c91334bace3b9168939eead3d6b
Morten Petersen <morten@mscp.dk>
2021-01-07 14:46:28 +0200

    updating filter and small fixes

--- src/screens/homeScreen.tsx
+++ src/screens/homeScreen.tsx
@@ -111,7 +111,7 @@ class FilterDropDown extends React.Component<FilterDropDownProps, FilterDropDown
     render() {
 
         return (
-            <ScrollView>
+            <ScrollView style={{ maxHeight: 500 }}>
                 <ListItem key={0} bottomDivider containerStyle={{ padding: 12 }} onPress={() => this.updateView(1)}>
                     <ListItem.Title>1 mile radius</ListItem.Title>
                     <ListItem.CheckBox size={18} checkedIcon='check' uncheckedIcon='check' uncheckedColor='white' checked={this.state.radius == 1} />
@@ -136,7 +136,59 @@ class FilterDropDown extends React.Component<FilterDropDownProps, FilterDropDown
                     <ListItem.Title>50 miles radius</ListItem.Title>
                     <ListItem.CheckBox size={18} checkedIcon='check' uncheckedIcon='check' uncheckedColor='white' checked={this.state.radius == 50} />
                 </ListItem>
+                <ListItem containerStyle={{ backgroundColor: '#eee' }} style={{ height: 5, backgroundColor: 'transparent' }}></ListItem>
 
+                <ListItem key={6} bottomDivider containerStyle={{ padding: 12 }} onPress={() => this.props.updateSorting('distance')}>
+                    <ListItem.Title>Sort By Distance</ListItem.Title>
+                    {this.props.sortingType == 'distance' && (
+                        <ListItem.CheckBox size={18} checkedIcon='chevron-up' uncheckedIcon='chevron-down' checked={this.props.sortingDirection == 1} />
+                    )}
+                </ListItem>
+                <ListItem key={7} bottomDivider containerStyle={{ padding: 12 }} onPress={() => this.props.updateSorting('birthday')}>
+                    <ListItem.Title>Sort By Birthday</ListItem.Title>
+                    {this.props.sortingType == 'birthday' && (
+                        <ListItem.CheckBox size={18} checkedIcon='chevron-up' uncheckedIcon='chevron-down' checked={this.props.sortingDirection == 1} />
+                    )}
+                </ListItem>
+
+                <ListItem containerStyle={{ backgroundColor: '#eee' }} style={{ height: 5, backgroundColor: 'transparent' }}></ListItem>
+
+                <ListItem key={8} bottomDivider containerStyle={{ padding: 12 }} onPress={() => this.updateMonth(1)}>
+                    <ListItem.Title>1 month</ListItem.Title>
+                    <ListItem.CheckBox size={18} checkedIcon='check' uncheckedIcon='check' uncheckedColor='white' checked={this.state.months == 1} />
+                </ListItem>
+                <ListItem key={9} bottomDivider containerStyle={{ padding: 12 }} onPress={() => this.updateMonth(2)}>
+                    <ListItem.Title>2 months</ListItem.Title>
+                    <ListItem.CheckBox size={18} checkedIcon='check' uncheckedIcon='check' uncheckedColor='white' checked={this.state.months == 2} />
+                </ListItem>
+                <ListItem key={10} bottomDivider containerStyle={{ padding: 12 }} onPress={() => this.updateMonth(3)}>
+                    <ListItem.Title>3 months</ListItem.Title>
+                    <ListItem.CheckBox size={18} checkedIcon='check' uncheckedIcon='check' uncheckedColor='white' checked={this.state.months == 3} />
+                </ListItem>
+                <ListItem key={11} bottomDivider containerStyle={{ padding: 12 }} onPress={() => this.updateMonth(4)}>
+                    <ListItem.Title>4 months</ListItem.Title>
+                    <ListItem.CheckBox size={18} checkedIcon='check' uncheckedIcon='check' uncheckedColor='white' checked={this.state.months == 4} />
+                </ListItem>
+                <ListItem key={12} bottomDivider containerStyle={{ padding: 12 }} onPress={() => this.updateMonth(5)}>
+                    <ListItem.Title>5 months</ListItem.Title>
+                    <ListItem.CheckBox size={18} checkedIcon='check' uncheckedIcon='check' uncheckedColor='white' checked={this.state.months == 5} />
+                </ListItem>
+                <ListItem key={13} bottomDivider containerStyle={{ padding: 12 }} onPress={() => this.updateMonth(6)}>
+                    <ListItem.Title>6 months</ListItem.Title>
+                    <ListItem.CheckBox size={18} checkedIcon='check' uncheckedIcon='check' uncheckedColor='white' checked={this.state.months == 6 || this.state.months == 0} />
+                </ListItem>
+                <ListItem key={14} bottomDivider containerStyle={{ padding: 12 }} onPress={() => this.updateMonth(7)}>
+                    <ListItem.Title>7 months</ListItem.Title>
+                    <ListItem.CheckBox size={18} checkedIcon='check' uncheckedIcon='check' uncheckedColor='white' checked={this.state.months == 7} />
+                </ListItem>
+                <ListItem key={15} bottomDivider containerStyle={{ padding: 12 }} onPress={() => this.updateMonth(8)}>
+                    <ListItem.Title>8 months</ListItem.Title>
+                    <ListItem.CheckBox size={18} checkedIcon='check' uncheckedIcon='check' uncheckedColor='white' checked={this.state.months == 8} />
+                </ListItem>
+                <ListItem key={16} bottomDivider containerStyle={{ padding: 12 }} onPress={() => this.updateMonth(9)}>
+                    <ListItem.Title>9 months</ListItem.Title>
+                    <ListItem.CheckBox size={18} checkedIcon='check' uncheckedIcon='check' uncheckedColor='white' checked={this.state.months == 9} />
+                </ListItem>
             </ScrollView>
         );
     }
@@ -182,7 +234,7 @@ export class HomeScreen extends React.Component<HomeProps, HomeState> {
 
         this.setStartLocation()
 
-        this.state = { leads: leads, isLoading: true, activeView: 0, filterDistance: 5, filterMonths: 6, savingLead: false, showRadiusFilter: false, leadSortingType: 'distance', leadSortingDirection: 1 };
+        this.state = { leads: leads, isLoading: true, activeView: 0, filterDistance: 5, filterMonths: 3, savingLead: false, showRadiusFilter: false, leadSortingType: 'distance', leadSortingDirection: 1 };
     }
 
     async setStartLocation() {
@@ -267,7 +319,6 @@ export class HomeScreen extends React.Component<HomeProps, HomeState> {
                 this.setState({ isLoading: false })
             })
         })
-
     }
 
     sortLeads(leads: Lead[], sortingType: string, direction: number): Lead[] {
@@ -616,7 +667,7 @@ export class HomeScreen extends React.Component<HomeProps, HomeState> {
                             <View style={[{ flexDirection: 'row', alignItems: 'center' }]}>
                                 <View style={[{ flex: 4, flexDirection: 'column' }]}>
                                     <Text style={styles.titleText}>{this.state.activeLead?.firstname} {this.state.activeLead?.lastName}</Text>
-                                    <Text style={{ fontSize: 16, color: 'gray', marginTop: 1 }}>{this.state.activeLead?.distance.toFixed(1)} miles away</Text>
+                                    <Text style={{ fontSize: 16, color: 'gray', marginTop: 1 }}>{(Math.round((this.state.activeLead?.distance || 0) * 10)/10)} miles away</Text>
                                     <Text style={{ fontSize: 18, fontWeight: '600', marginTop: 10 }}>Address</Text>
                                     <Text style={{ fontSize: 16, color: 'gray', marginTop: 5 }}>{this.state.activeLead?.address}</Text>
                                     <Text style={{ fontSize: 16, color: 'gray' }}>{this.state.activeLead?.city}</Text>
@@ -741,7 +792,7 @@ export class HomeScreen extends React.Component<HomeProps, HomeState> {
 
                                             <Text style={{ color: 'grey' }}>
 
-                                                {Math.round(lead.distance) + ' mi. away'}
+                                                {Math.round(lead.distance * 10) / 10 + ' mi. away'}
                                             </Text>
                                         </ListItem.Subtitle>
 
@@ -756,6 +807,8 @@ export class HomeScreen extends React.Component<HomeProps, HomeState> {
                                 <View style={[{ flexDirection: 'row', alignItems: 'center' }]}>
                                     <View style={[{ flex: 4, flexDirection: 'column' }]}>
                                         <Text style={styles.titleText}>{this.state.activeLead?.firstname} {this.state.activeLead?.lastName}</Text>
+                                        <Text style={{ fontSize: 16, color: 'gray', marginTop: 1 }}>{(Math.round((this.state.activeLead?.distance || 0) * 10)/10)} miles away</Text>
+
                                         <Text style={{ fontSize: 16, color: 'gray', marginTop: 5 }}>{this.state.activeLead?.address}</Text>
                                         <Text style={{ fontSize: 16, color: 'gray' }}>{this.state.activeLead?.city}</Text>
                                         <Text style={{ fontSize: 16, color: 'gray' }}>{this.state.activeLead?.zipCode} {this.state.activeLead?.county}</Text>
